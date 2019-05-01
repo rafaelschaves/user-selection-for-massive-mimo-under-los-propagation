a7d15efc5bea450d743b701b201cd4d86b01f517
Rafael Chaves <rflschaves@poli.ufrj.br>
2019-05-01 18:10:07 +1000

    Add correlation-based selection

--- functions/userScheduling.m
+++ functions/userScheduling.m
@@ -55,6 +55,51 @@ switch algorithm
         
         sel_chnl_mtx = chnl_mtx;
         sel_chnl_mtx(:,user_set_drop) = [];
+    case 'CORRELATION-BASED SELECTION'
+        chnl_mtx_norm = zeros(n_antenna,n_user);
+        user_set_drop = zeros(n_user - n_selected,1);
+        
+        eye_K = eye(n_user);
+        
+        chnl_mtx_aux = chnl_mtx;
+                     
+        for i = 1:n_user-n_selected
+            for k = 1:n_user
+                norm_h_k = norm(chnl_mtx_aux(:,k),2);
+            
+                if(norm_h_k == 0)
+                    chnl_mtx_norm(:,k) = 0;
+                else
+                    chnl_mtx_norm(:,k) = chnl_mtx_aux(:,k)/norm_h_k;
+                end
+            end
+                    
+            corr_mtx     = abs(chnl_mtx_norm'*chnl_mtx_norm);
+            corr_mtx_aux = corr_mtx - eye_K;
+            
+            [~,corr_idx] = max(corr_mtx_aux(:));
+            [corr_i,corr_j] = ind2sub(size(corr_mtx_aux),corr_idx);
+            
+            h_i = corr_mtx_aux(:,corr_i);
+            h_j = corr_mtx_aux(:,corr_j);
+            
+            h_i(corr_j) = [];
+            h_j(corr_i) = [];
+            
+            if(max(h_i) > max(h_j))
+                user_set_drop(i) = corr_i;
+            else
+                user_set_drop(i) = corr_j;
+            end
+            
+            chnl_mtx_aux(:,user_set_drop(i)) = zeros(n_antenna,1);
+        end
+        
+        user_set_sel = (1:n_user)';
+        user_set_sel(user_set_drop) = [];
+
+        sel_chnl_mtx = chnl_mtx;
+        sel_chnl_mtx(:,user_set_drop) = [];
     case 'ICI-BASED SELECTION'
         user_set_drop = zeros(n_user - n_selected,1);
         
