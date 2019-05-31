63057c7510ca8395f08592996911671f9e51ef07
Rafael Chaves <rflschaves@poli.ufrj.br>
2019-05-31 13:56:04 +1000

    Update shell script

--- run_simulation_fixed_user_selection.sh
+++ run_simulation_fixed_user_selection.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 MC=10000
+M=64
+K=18
 L=13
 CHANNEL=('ur-los' 'rayleigh')
 
 for iDX in 0 1
 do
-    for M in 64 256
-    do
-        for K in 18 36 72
+    #for M in 64 256
+    #do
+        for SNR in -20 -15 -10 -5 0 5 10 
         do
-            for SNR in -20 -15 -10 -5 0 5 10 
-            do
-                ARGUMENTS="MC = $MC; M = $M; K = $K; L = $L; snr_db = $SNR; channel_type = ${CHANNEL[$IDX]};"
-                nice -n 19 nohup matlab -nojvm -r $ARGUMENTS <user_selection.m> output_${CHANNEL[$IDX]}_$M_$K_$L_$SNR.log &
-            done
+            ARGUMENTS="MC = $MC; M = $M; K = $K; L = $L; snr_db = $SNR; channel_type = ${CHANNEL[$IDX]};"
+            nice -n 19 nohup matlab -nojvm -r $ARGUMENTS <user_selection.m> output_${CHANNEL[$IDX]}_$SNR.log &
         done
-    done
+    #done
 done
