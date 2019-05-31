df0676bd8471f380fc5d04455aab8a774be41355
Rafael Chaves <rflschaves@poli.ufrj.br>
2019-05-31 13:34:35 +1000

    Fix some bugs

--- run_simulation_fixed_user_selection.sh
+++ run_simulation_fixed_user_selection.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-MC = 10000
-L = 13
-channel_type = ('ur-los' 'rayleigh')
+MC=10000
+L=13
+CHANNEL=('ur-los' 'rayleigh')
 
-for idx_channel in 0 1
+for iDX in 0 1
 do
     for M in 64 256
     do
         for K in 18 36 72
         do
-            for snr in -20 -15 -10 -5 0 5 10 
+            for SNR in -20 -15 -10 -5 0 5 10 
             do
-                arguments = "MC = $MC; M = $M; K = $K; L = $L; snr_db = $snr; channel_type = ${channel_type[$idx_channel]};"
-                nice -n 19 nohup matlab -nojvm -r $arguments <user_selection.m> output_${channel_type[$idx_channel]}_$M_$K_$L_$snr.log &
+                arguments = "MC = $MC; M = $M; K = $K; L = $L; snr_db = $SNR; channel_type = ${CHANNEL[$IDX]};"
+                nice -n 19 nohup matlab -nojvm -r $arguments <user_selection.m> output_${CHANNEL[$IDX]}_$M_$K_$L_$SNR.log &
             done
         done
     done
