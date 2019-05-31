00b5b33e210273baf28e36005326833abf40e6c1
Rafael Chaves <rflschaves@poli.ufrj.br>
2019-05-31 16:16:03 +1000

    Test scripts

--- run_simulation_fixed_user_selection.sh
+++ run_simulation_fixed_user_selection.sh
@@ -4,10 +4,10 @@ MC=10000
 M=64
 K=18
 L=13
-CHANNEL="ur-los"
+CHANNEL=ur-los
 
 for SNR in -20 -15 -10 -5 0 5 10 
 do
-    ARGUMENTS="MC=$MC; M=$M; K=$K; L=$L; snr_db=$SNR; channel_type=$CHANNEL;"
+    ARGUMENTS="MC=$MC; M=$M; K=$K; L=$L; snr_db=$SNR; channel_type='$CHANNEL';"
     nice -n 19 nohup matlab -nojvm -r $ARGUMENTS <user_selection.m> ./log/output_$SNR.log &
 done
