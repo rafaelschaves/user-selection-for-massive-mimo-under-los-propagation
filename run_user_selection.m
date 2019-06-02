52cbb0a9b660e8eea047326f88330b82b03cdc07
Rafael Chaves <rflschaves@poli.ufrj.br>
2019-06-03 09:56:09 +1000

    Change parameters

--- run_user_selection.m
+++ run_user_selection.m
@@ -1,9 +1,9 @@
 MC = 10000;                                                                % Size of the outer Monte Carlo ensemble (Varies the channel realizarions)
-L = 67;                                                                    % Number of selected users
+L = 31;                                                                    % Number of selected users
 channel_type = 'rayleigh';
 
 for M = [64 256]                                                           % Number of antennas at the base station
-    for K = [72]                                                     % Number of users at the cell
+    for K = [36 72]                                                     % Number of users at the cell
         for snr_db = [-20 -15 -10 -5 0 5 10]                               % SNR in dB
             run user_selection.m 
         end
