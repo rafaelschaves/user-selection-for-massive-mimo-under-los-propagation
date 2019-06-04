18d3452576ee758fb619d6bb289a0c7e48c6db71
Rafael Chaves <rflschaves@poli.ufrj.br>
2019-06-04 14:27:21 +1000

    Change parameters

--- run_automatic_selection.m
+++ run_automatic_selection.m
@@ -1,14 +1,15 @@
 MC = 10000;                                                                % Size of the outer Monte Carlo ensemble (Varies the channel realizarions)
 
+snr_db = 10;                                                               % SNR in dB
 channel_model = {'ur-los','rayleigh'};                                     % Channel models
 
 for chn_idx = [1 2]
     channel_type = channel_model{chn_idx};
     for M = [64 256]                                                       % Number of antennas at the base station
         for K = [18 36 72]                                                 % Number of users at the cell
-            for snr_db = [-20 -15 -10 -5 0 5 10]                           % SNR in dB
+            % for snr_db = [-20 -15 -10 -5 0 5 10]                           % SNR in dB
                 run automatic_selection.m 
-            end
+            % end
         end
     end
 end
