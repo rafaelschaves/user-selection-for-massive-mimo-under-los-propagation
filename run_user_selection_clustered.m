fc312f7441f2fd17971d296471c94c3145c257e6
Rafael Chaves <rflschaves@poli.ufrj.br>
2019-06-11 16:05:01 +1000

    Create run_user_selection_clustered.m

---
+++ run_user_selection_clustered.m
@@ -0,0 +1,13 @@
+MC = 10000;                                                                % Size of the outer Monte Carlo ensemble (Varies the channel realizarions)
+L = 13;                                                                    % Number of selected users
+snr_db = 10;                                                               % SNR in dB
+
+for M = [64 256]                                                           % Number of antennas at the base station
+    for K = [36 72]                                                        % Number of users at the cell
+        for theta_mid = [0 pi/4 pi/2]
+            for theta_step = [pi/180 pi/18 pi/9 pi/6 pi/2] 
+                run user_selection_clustered.m
+            end
+        end
+    end
+end
