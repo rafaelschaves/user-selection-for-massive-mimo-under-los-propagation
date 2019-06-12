fdb19a14926d348bce046cfd091907649beb2a4c
Rafael Chaves <rflschaves@poli.ufrj.br>
2019-06-12 11:49:20 +1000

    Rename rateCalculation.m to throughput.m

---
+++ functions/throughput.m
@@ -0,0 +1,43 @@
+% Write a summary and a help
+function [thrput,varargout] = throughput(chnl_mtx,proc_mtx,pow_vec,settings)
+
+tau_c     = settings.coherenceTime;                                        % Coherence time in samples
+tau_p     = settings.PilotTime;                                            % Pilot time in samples
+ratio     = settings.uplinkDownlinkTimeRatio;                              % Ratio between the uplink and downlink payload time
+bandwidth = settings.bandwidth;                                            % Sytem bandwidth in Hz
+cell_area = settings.cellArea;                                             % Cell area in km^2
+snr       = settings.snr;                                                  % SNR
+link      = settings.linkType;                                             % Link type (e.g. uplink or Downlink)
+
+gamma  = sinr(chnl_mtx,proc_mtx,pow_vec,snr,link);                         % SINR
+se     = ratio*(1 - tau_p/tau_c)*log2(1 + gamma);                          % Spectral efficiency in b/s/Hz/cell
+thrput = bandwidth*se/cell_area;                                           % Throughput in b/s/km^2
+
+varargout{1} = gamma;
+varargout{2} = se;
+
+end
+
+function [gamma] = sinr(chnl_mtx,proc_mtx,pow_vec,snr,link)
+
+link = upper(link);
+
+switch link
+    case 'UPLINK'
+        aux_mtx        = abs(proc_mtx'*chnl_mtx).^2;
+        pow_signal_vec = pow_vec.*diag(aux_mtx);
+        pow_interf_vec = sum(pow_vec.*aux_mtx,2) - pow_signal_vec;
+        proc_mtx_norm  = vecnorm(proc_mtx,2).^2;
+        
+        gamma = (snr.*pow_signal_vec)./(proc_mtx_norm' + snr.*pow_interf_vec);
+    case 'DOWNLINK'
+        aux_mtx        = abs(chnl_mtx.'*proc_mtx).^2;
+        pow_signal_vec = pow_vec.*diag(aux_mtx);
+        pow_interf_vec = sum(pow_vec.*aux_mtx,2) - pow_signal_vec;
+        
+        gamma = (snr.*pow_signal_vec)./(1 + snr.*pow_interf_vec);
+    otherwise
+        error('Invalid link');
+end
+
+end
