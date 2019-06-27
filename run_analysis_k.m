e51347c651671481032ff366be16d7967e0258f6
Rafael Chaves <rflschaves@poli.ufrj.br>
2019-06-27 17:55:58 +1000

    Change parameters

--- run_analysis_k.m
+++ run_analysis_k.m
@@ -2,7 +2,7 @@ MC = 10000;                                                                % Siz
 
 snr_db = 10;
 
-channel_type = 'rayleigh';
+channel_type = 'ur-los';
 
 for M = [64 128 256]                                                       % Number of antennas at the base station
     for K = [1 2 4 8 16 32 64 128 256 512 1024]                            % Number of users at the cell
