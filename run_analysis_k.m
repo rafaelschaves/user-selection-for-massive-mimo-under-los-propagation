56aac1fb6b40f28732a7b3762c1f9544b1e40262
Rafael Chaves <rflschaves@poli.ufrj.br>
2019-06-27 16:59:26 +1000

    Create analysis_k.m and run_analysis_k.m

---
+++ run_analysis_k.m
@@ -0,0 +1,11 @@
+MC = 10000;                                                                % Size of the outer Monte Carlo ensemble (Varies the channel realizarions)
+
+snr_db = 10;
+
+channel_type = 'rayleigh';
+
+for M = [64 128 256]                                                       % Number of antennas at the base station
+    for K = [1 2 4 8 16 32 64 128 256 512 1024]                            % Number of users at the cell
+        run analysis_k
+    end
+end
