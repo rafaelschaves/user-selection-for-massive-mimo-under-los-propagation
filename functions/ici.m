7c00e5e198b6dc72b0cf495b9a0d1118d3cb9e9e
Rafael Chaves <rflschaves@poli.ufrj.br>
2019-11-15 10:24:04 +1100

    Change the code in order to decrease the simulation time

--- functions/ici.m
+++ functions/ici.m
@@ -4,18 +4,12 @@ n_antenna  = size(chnl_mtx,1);
 n_user     = size(chnl_mtx,2);
 n_user_aux = size(chnl_mtx,2) - length(find(all(chnl_mtx == 0)));
 
-chnl_mtx_norm = zeros(n_antenna,n_user);
-
 norm_h_k = vecnorm(chnl_mtx,2);
+idx_aux  = find(norm_h_k ~= 0);
 
-for k = 1:n_user    
-    if(norm_h_k(k) ~= 0)
-        chnl_mtx_norm(:,k) = chnl_mtx(:,k)/norm_h_k;
-    end
-end
+chnl_mtx_norm            = zeros(n_antenna,n_user);
+chnl_mtx_norm(:,idx_aux) = chnl_mtx(:,idx_aux)./norm_h_k(idx_aux);
 
-interf_mtx_norm = chnl_mtx_norm'*chnl_mtx_norm;
-
-psi = (sum(abs(interf_mtx_norm),2) - 1)/(n_user_aux - 1);
+psi = (sum(abs(chnl_mtx_norm'*chnl_mtx_norm),2) - 1)/(n_user_aux - 1);
 
 end
