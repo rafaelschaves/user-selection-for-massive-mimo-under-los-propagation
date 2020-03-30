6d3b27145317d00a2ad6574cc0f89008def4fb34
Rafael Chaves <rflschaves@poli.ufrj.br>
2020-03-30 14:52:26 +1100

    Include the MMSE precoding option

--- functions/DLspectralEfficiency.m
+++ functions/DLspectralEfficiency.m
@@ -36,7 +36,7 @@ se_mr    = (tau_d/tau_c)*log2(1 + gamma_mr);
 if nargout > 1
     if size(H,3) == 1
         H_zf = H;
-    elseif size(H,3) == 2
+    elseif size(H,3) > 1
         H_zf = H(:,:,2);
     end
     
@@ -63,6 +63,46 @@ if nargout > 1
     se_zf    = (tau_d/tau_c)*log2(1 + gamma_zf);
     
     varargout{1} = se_zf;
+end
+
+% MMSE Processing
+
+if nargout > 2
+    if size(H,3) == 1
+        H_mmse = H;
+    elseif size(H,3) == 2
+        error('Invalid channel matrix size');
+    elseif size(H,3) > 2
+        H_mmse = H(:,:,3);
+    end
+    
+    if size(beta,2) == 1
+        beta_mmse = beta;
+    elseif size(beta,2) == 2
+        error('Invalid large-scale vector size');
+    elseif size(beta,2) > 2
+        beta_mmse = beta(:,2);
+    end
+    
+    if size(eta,2) == 1
+        eta_mmse = eta;
+    elseif size(eta,2) == 2
+        error('Invalid power allocation vector size');
+    elseif size(eta,2) > 2
+        eta_mmse = eta(:,3);
+    end
+    
+    V_mmse      = conj(H_mmse)/(H_mmse.'*conj(H_mmse) + eye(K)./(beta*rho));
+    v_mmse_norm = vecnorm(V_mmse);
+    
+    W_mmse = V_mmse*diag(1./v_mmse_norm);
+    
+    [P_s,P_i] = computeSignalAndInterferencePower(H_mmse,beta_mmse,rho,eta_mmse,W_mmse);
+    
+    gamma_mmse = P_s./(P_i + 1);
+    se_mmse    = (tau_d/tau_c)*log2(1 + gamma_mmse);
+    
+    varargout{2} = se_mmse;
 end
 
 end
