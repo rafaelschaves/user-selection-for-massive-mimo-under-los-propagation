7f8879a454e7fd98e32fe6309e2bb580c5c1dccc
Rafael Chaves <rflschaves@poli.ufrj.br>
2020-05-04 18:33:25 +1000

    Change parameters for simulation

--- run_user_selection_2.m
+++ run_user_selection_2.m
@@ -8,13 +8,13 @@ MC = 1000;                                                                 % Siz
 
 channel_type = 'rayleigh';
 
-M = 50;                                                                   % Number of antennas at the base station
+M = 100;                                                                   % Number of antennas at the base station
 
 % M = 50  and K = [10 25 50 75]
 % M = 100 and K = [10 25 50 75 100 150]
 % M = 200 and K = [10 25 50 75 100 150 200 250]
 
-for K = [10 25 50 75]                                              % Number of users at the cell
+for K = [10 25 50 75 100 150]                                              % Number of users at the cell
     K
     run user_selection_2.m
 end
