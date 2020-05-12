40f520f5d4cb4f7497ddcb030d4914a0de40319f
Rafael Chaves <rflschaves@poli.ufrj.br>
2020-05-12 17:28:08 +1000

    Change paarameters for simulation

--- run_user_selection_2.m
+++ run_user_selection_2.m
@@ -4,17 +4,17 @@ clc;
 
 rng('shuffle');
 
-MC = 1000;                                                                 % Size of the outer Monte Carlo ensemble (Varies the channel realizarions)
+MC = 500;                                                                 % Size of the outer Monte Carlo ensemble (Varies the channel realizarions)
 
-channel_type = 'rayleigh';
+channel_type = 'ur-los';
 
-M = 100;                                                                   % Number of antennas at the base station
+M = 200;                                                                   % Number of antennas at the base station
 
 % M = 50  and K = [10 25 50 75]
 % M = 100 and K = [10 25 50 75 100 150]
 % M = 200 and K = [10 25 50 75 100 150 200 250]
 
-for K = [100 150]                                              % Number of users at the cell
+for K = [200 250]                                              % Number of users at the cell
     K
     run user_selection_2.m
 end
