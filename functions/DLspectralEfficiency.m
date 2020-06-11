c79f9714a3dff22b4aeb738b3f0d9aa63bfede35
Rafael Chaves <rflschaves@poli.ufrj.br>
2020-06-11 15:45:26 +1000

    Calculate spectral efficiency when partial CSI is available

--- functions/DLspectralEfficiency.m
+++ functions/DLspectralEfficiency.m
@@ -1,27 +1,33 @@
 function [se_mr,varargout] = DLspectralEfficiency(H,beta,rho,eta,varargin)
 
-N_ARGIN = 6;
+N_ARGIN = 7;
 
-if nargin == N_ARGIN-2
+if nargin == N_ARGIN-3
+    H_hat = H;
+    tau_d = 1;
+    tau_c = 1;
+elseif nargin == N_ARGIN-2
+    H_hat = varargin{1};
     tau_d = 1;
     tau_c = 1;
 elseif nargin == N_ARGIN-1
-    tau_d = varargin{1};
-    tau_c = 200;
+    tau_d = varargin{2};
+    tau_c = 1000;
 elseif nargin == N_ARGIN
-    tau_d = varargin{1};
-    tau_c = varargin{2};
+    tau_d = varargin{2};
+    tau_c = varargin{3};
 end
 
 K = size(H,2);
 
 % MR Processing
 
-eta_mr  = eta(:,1);             % Power allocation vector for MR processing
-H_mr    = H(:,:,1);
-beta_mr = beta(:,1);
+eta_mr   = eta(:,1);                                                       % Power allocation vector for MR processing
+H_mr     = H(:,:,1);
+H_mr_hat = H_hat(:,:,1);
+beta_mr  = beta(:,1);
 
-V_mr      = conj(H_mr);
+V_mr      = conj(H_mr_hat);
 v_mr_norm = vecnorm(V_mr);
 
 W_mr = V_mr*diag(1./v_mr_norm);
@@ -35,9 +41,11 @@ se_mr    = (tau_d/tau_c)*log2(1 + gamma_mr);
 
 if nargout > 1
     if size(H,3) == 1
-        H_zf = H;
+        H_zf     = H;
+        H_zf_hat = H_hat;
     elseif size(H,3) > 1
-        H_zf = H(:,:,2);
+        H_zf     = H(:,:,2);
+        H_zf_hat = H_hat(:,:,2);
     end
     
     if size(beta,2) == 1
@@ -52,7 +60,7 @@ if nargout > 1
         eta_zf = eta(:,2);
     end
     
-    V_zf      = conj(H_zf)/(H_zf.'*conj(H_zf) + 1e-9*eye(K));
+    V_zf      = conj(H_zf_hat)/(H_zf_hat.'*conj(H_zf_hat) + 1e-9*eye(K));
     v_zf_norm = vecnorm(V_zf);
     
     W_zf = V_zf*diag(1./v_zf_norm);
@@ -69,11 +77,13 @@ end
 
 if nargout > 2
     if size(H,3) == 1
-        H_mmse = H;
+        H_mmse     = H;
+        H_mmse_hat = H_hat;
     elseif size(H,3) == 2
         error('Invalid channel matrix size');
     elseif size(H,3) > 2
-        H_mmse = H(:,:,3);
+        H_mmse     = H(:,:,3);
+        H_mmse_hat = H_hat(:,:,3);
     end
     
     if size(beta,2) == 1
@@ -92,7 +102,7 @@ if nargout > 2
         eta_mmse = eta(:,3);
     end
     
-    V_mmse      = conj(H_mmse)/(H_mmse.'*conj(H_mmse) + eye(K)./(beta*rho));
+    V_mmse      = conj(H_mmse_hat)/(H_mmse_hat.'*conj(H_mmse_hat) + eye(K)./(beta*rho));
     v_mmse_norm = vecnorm(V_mmse);
     
     W_mmse = V_mmse*diag(1./v_mmse_norm);
